37c99074b01be14ea19c054d4a7f3a86ee2a8897
Poly Stefane <polyvstefane@gmail.com>
2024-10-31 01:29:16 -0300

    feat: add news to forms

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 
 function App() {
   const [values, setValues] = useState({});
-  const [followers, setFollowers] = useState([]); 
-  const [editingId, setEditingId] = useState(null); 
+  const [followers, setFollowers] = useState([]);
+  const [editingId, setEditingId] = useState(null);
 
   const handleChangeValues = (value) => {
     setValues((prevValue) => ({
@@ -19,17 +19,17 @@ function App() {
       axios.put(`http://localhost:3001/followers/${editingId}`, values)
         .then(() => {
           console.log("Follower atualizado:", values);
-          setEditingId(null); 
-          setValues({}); 
-          getFollowers(); 
+          setEditingId(null);
+          setValues({});
+          getFollowers();
         })
         .catch((error) => console.error("Erro ao atualizar follower:", error));
     } else {
       axios.post('http://localhost:3001/followers', values)
         .then(() => {
           console.log("Follower inserido:", values);
-          setValues({}); 
-          getFollowers(); 
+          setValues({});
+          getFollowers();
         })
         .catch((error) => console.error("Erro ao inserir follower:", error));
     }
@@ -46,15 +46,15 @@ function App() {
   }, []);
 
   const handleEdit = (follower) => {
-    setValues(follower); 
-    setEditingId(follower.id); 
+    setValues(follower);
+    setEditingId(follower.id);
   };
 
   const handleDelete = (id) => {
     axios.delete(`http://localhost:3001/followers/${id}`)
       .then(() => {
         console.log("Follower excluído:", id);
-        getFollowers(); 
+        getFollowers();
       })
       .catch((error) => console.error("Erro ao excluir follower:", error));
   };
@@ -64,43 +64,119 @@ function App() {
       <div className="register-container">
         <h1 className="register-title">Lamb Followers</h1>
 
-        <input 
-          type="text"
-          name="name"
-          placeholder="Nome"
-          className="register-input"
-          value={values.name || ''}
-          onChange={handleChangeValues}
-        />
-        <input 
+        <div className="input-group">
+          <p>Nome:</p>
+          <input
+            type="text"
+            name="name"
+            placeholder="Digite o nome do seguidor"
+            className="register-input"
+            value={values.name || ''}
+            onChange={handleChangeValues}
+          />
+       </div>
+
+      <div className="input-group">
+        <p>Gênero:</p>
+        <input
           type="text"
           name="gender"
-          placeholder="Gênero"
+          placeholder="Digite o gênero do seguidor"
           className="register-input"
           value={values.gender || ''}
           onChange={handleChangeValues}
         />
-        <input 
-          type="text"
-          name="occupation"
-          placeholder="Ocupação"
-          className="register-input"
-          value={values.occupation || ''}
-          onChange={handleChangeValues}
-        />
-        <input 
-          type="text"
-          name="nivel"
-          placeholder="Nível de Devoção"
-          className="register-input"
-          value={values.nivel || ''}
-          onChange={handleChangeValues}
-        />
-        <button 
-          className="register-button" 
+      </div>
+
+      <div className="input-group">
+          <p>Nivel de devoção:</p>
+          <input
+            type="number"
+            name="nivel"
+            placeholder="Nível de Devoção"
+            className="register-input"
+            value={values.nivel || ''}
+            onChange={handleChangeValues}
+          />
+        </div>
+
+        <div className="radio-group">
+          <p>Ocupação:</p>
+          <label>
+            <input
+              type="radio"
+              name="occupation"
+              value="Coletar"
+              checked={values.occupation === "Coletar"}
+              onChange={handleChangeValues}
+            />Coletar
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="occupation"
+              value="Construir"
+              checked={values.occupation === "Construir"}
+              onChange={handleChangeValues}
+            /> Construir
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="occupation"
+              value="Explorar"
+              checked={values.occupation === "Explorar"}
+              onChange={handleChangeValues}
+            /> Explorar
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="occupation"
+              value="Orar"
+              checked={values.occupation === "Orar"}
+              onChange={handleChangeValues}
+            /> Orar
+          </label>
+        </div>
+
+        
+        <div className="radio-group">
+          <p>Alinhamento com o Culto:</p>
+          <label>
+            <input
+              type="radio"
+              name="alignment"
+              value="Leal"
+              checked={values.alignment === "Leal"}
+              onChange={handleChangeValues}
+            /> Leal
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="alignment"
+              value="Neutro"
+              checked={values.alignment === "Neutro"}
+              onChange={handleChangeValues}
+            /> Neutro
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="alignment"
+              value="Rebelde"
+              checked={values.alignment === "Rebelde"}
+              onChange={handleChangeValues}
+            /> Rebelde
+          </label>
+        </div>
+
+        <button
+          className="register-button"
           onClick={handleClickButton}
         >
-          {editingId ? "Atualizar no Culto" : "Inserir no Culto"}
+          {editingId ? "Atualizar Seguidor" : "Participar do Culto"}
         </button>
       </div>
 
@@ -109,10 +185,11 @@ function App() {
         <ul>
           {followers.map((follower) => (
             <li key={follower.id}>
-              <strong>Nome:</strong> {follower.name} | 
-              <strong> Gênero:</strong> {follower.gender} | 
-              <strong> Ocupação:</strong> {follower.occupation} | 
-              <strong> Nível:</strong> {follower.nivel} 
+              <strong>Nome:</strong> {follower.name} |
+              <strong> Gênero:</strong> {follower.gender} |
+              <strong> Profissão:</strong> {follower.occupation} |
+              <strong> Nível:</strong> {follower.nivel} |
+              <strong> Alinhamento:</strong> {follower.alignment}
               <button className="edit-button" onClick={() => handleEdit(follower)}>Editar</button>
               <button className="delete-button" onClick={() => handleDelete(follower.id)}>Excluir</button>
             </li>
